caeb57f2544d5ec8fd6f4cde61bee99d2ad6be66
YatagacLapotk <you@example.com>
2024-10-08 15:19:42 +0300

    modified:   M2/AAC2M2P2/AAC2M2P2.vhd

--- M2/AAC2M2P2/AAC2M2P2.vhd
+++ M2/AAC2M2P2/AAC2M2P2.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
 
 ENTITY RAM128_32 IS
 	PORT
@@ -11,3 +12,18 @@ ENTITY RAM128_32 IS
 		q		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 	);
 END RAM128_32;
+
+architecture RAM128_32_arch of RAM128_32 is
+	type memory is array  (0 to 127) of std_logic_vector(31 downto 0); 
+	signal ram_ad: memory:= (others=>(others=>'0'));
+begin
+	process(clock,wren)
+  	begin
+		if(rising_edge(clock)) then 
+			if(wren='1') then 
+				ram_ad(unsigned(address)) <= data;
+			end if;
+			q <= ram_ad(unsigned(address));
+		end if;
+	end process;
+end architecture;
