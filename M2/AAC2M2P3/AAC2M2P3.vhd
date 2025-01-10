c62efab8b3985c6f9bf10c3c4b7dc5cfb69d18b0
YatagacLapotk <you@example.com>
2025-01-11 00:21:45 +0300

    modified:   AAC2M2P3.vhd

--- M2/AAC2M2P3/AAC2M2P3.vhd
+++ M2/AAC2M2P3/AAC2M2P3.vhd
@@ -2,45 +2,57 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity FSM is
-generic(
-   A : std_logic_vector(1 downto 0):="00";
-   B : std_logic_vector(1 downto 0):="01";
-   C : std_logic_vector(1 downto 0):="10"
-);
-port (In1: in std_logic;
-   RST: in std_logic; 
-   CLK: in std_logic;
-   Out1 : inout std_logic);
+  port (
+    In1  : in std_logic;
+    RST  : in std_logic;
+    CLK  : in std_logic;
+    Out1 : inout std_logic);
 end FSM;
-architecture FSM_arch of FSM is 
-   signal cur,nex : std_logic_vector(1 downto 0);
+architecture FSM_arch of FSM is
+  type state_type is (A, B, C, D);
+  signal cur, nex : state_type;
 begin
-   comb_proc : process(In1) 
-   begin
-      case(cur) is 
-         when A => 
-            if (In1 = '0') then nex <= A;
-            elsif(In1 ='1') then nex <= B;
-            end if;
-            Out1 <= '0';
-         when B => 
-            if(In1 = '1') then nex <= B;
-            elsif(In1 = '0') then nex<= C;
-            end if;
-            Out1 <= '0';
-         when C => 
-            if (In1 = '0') then nex <= C;
-            elsif(In1 = '1') then nex <= A;
-            end if;
-            Out1 <= '1';
-         when others => nex <= A; 
-      end case;
-   end process comb_proc;
-   sync_proc : process(RST,CLK)
-   begin
-      if (RST = '1') then cur <= "00";
-      elsif (rising_edge(CLK)) then 
-         cur <=nex;
+  timing_proc : process (clk)
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
+        cur <= A;
+      else
+        cur <= nex;
       end if;
-   end process sync_proc;
+    end if;
+  end process timing_proc;
+  state_control : process (cur)
+  begin
+    case(cur)
+      when A =>
+         if In1 = '0'then
+            nex <= A;
+         elsif In1 = '1' then
+            nex <= B;
+         end if;
+      when B =>
+         if In1 = '1' then
+            nex <= B;
+         elsif In1 = '0' generate
+            nex <= C;
+         end if;
+      when C =>
+         if In1 = '0' then
+            nex <= C;
+         elsif In1 = '1' then
+            nex <= A;
+         end if;
+   end process state_control;
+   output_control : process (cur)
+   begin
+      case(cur)
+         when A =>
+            Out1 <= '0';
+         when B =>
+            Out1 <= '0';
+         when C =>
+            Out1 <= '1';
+      end case; 
+   end process;
 end architecture;
