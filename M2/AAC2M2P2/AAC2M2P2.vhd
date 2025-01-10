9ff54c557f82a559267b4c759b3f527805ac0744
YatagacLapotk <you@example.com>
2025-01-10 23:46:24 +0300

    modified:   AAC2M2P2.vhd

--- M2/AAC2M2P2/AAC2M2P2.vhd
+++ M2/AAC2M2P2/AAC2M2P2.vhd
@@ -1,29 +1,30 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-ENTITY RAM128_32 IS
-	PORT
-	(
-		address	: IN STD_LOGIC_VECTOR (6 DOWNTO 0);
-		clock		: IN STD_LOGIC  := '1';
-		data		: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-		wren		: IN STD_LOGIC ;
-		q		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
-	);
-END RAM128_32;
+entity RAM128_32 is
+  port (
+    address : in std_logic_vector (6 downto 0);
+    clock   : in std_logic := '1';
+    data    : in std_logic_vector (31 downto 0);
+    wren    : in std_logic;
+    q       : out std_logic_vector (31 downto 0)
+  );
+end RAM128_32;
 
 architecture RAM128_32_arch of RAM128_32 is
-	type memory is array  (0 to 127) of std_logic_vector(31 downto 0); 
-	signal ram_ad: memory:= (others=>(others=>'0'));
+  type memory is array (0 to 127) of std_logic_vector(31 downto 0);
+  signal ram_ad  : memory;
+  signal data_in : std_logic_vector(31 downto 0);
 begin
-	process(clock,wren)
-  	begin
-		if(rising_edge(clock)) then 
-			if(wren='1') then 
-				ram_ad(unsigned(address)) <= data;
-			end if;
-			q <= ram_ad(unsigned(address));
-		end if;
-	end process;
+  process (clock)
+  begin
+    if rising_edge(clock) then
+      if wren = '1' then
+        ram_ad(conv_integer(address)) <= data;
+      end if;
+      data_in <= ram_ad(conv_integer(address));
+    end if;
+  end process;
+  q <= data_in;
 end architecture;
